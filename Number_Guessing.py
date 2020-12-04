2baf1edea2ca4c561d0191d2bacc04178a6c92ca
Josiah Elza Jacob <63003688+josiahjacob@users.noreply.github.com>
2020-12-04 09:47:46 +0530

    Update Number_Guessing.py

--- Number_Guessing.py
+++ Number_Guessing.py
@@ -6,6 +6,8 @@ g=round(math.log(u - l + 1, 2))
 n=random.randint(l,u)
 print("You have only",g,"chances to guess")
 while g>0:
+    if g-1==0:
+        print("THIS IS YOUR LAST CHANCE")
     gn=int(input("Guess a no: "))
     if gn==n:
         print("Congratulations you did it ! ")
